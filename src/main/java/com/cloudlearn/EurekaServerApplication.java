4cc36bb24bf4d6f936a9b29e2bdcfd5efdd216f5
harrychan19 <shihai775532>
2019-04-20 07:37:16 +0800

    push

--- src/main/java/com/cloudlearn/EurekaServerApplication.java
+++ src/main/java/com/cloudlearn/EurekaServerApplication.java
@@ -1,8 +1,10 @@
 package com.cloudlearn;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,5 +14,10 @@ public class EurekaServerApplication {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
 
